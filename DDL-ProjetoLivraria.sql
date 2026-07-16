6ecabb0df384ddf7608a4749b3a76fec2dd69e9a
icaro-marra <icaromarra1@gmail.com>
2026-07-16 11:10:01 -0300

    Update and rename DDL-LivrariaProjetoFinal.sql to DDL-ProjetoLivraria.sql

--- DDL-ProjetoLivraria.sql
+++ DDL-ProjetoLivraria.sql
@@ -24,9 +24,9 @@ CREATE TABLE categorias (
 CREATE TABLE livros (
     id_livro INT AUTO_INCREMENT PRIMARY KEY,
     no_titulo VARCHAR(150) NOT NULL,
-    dt_publicacao INT NOT NULL,
-    vl_preco DECIMAL(10, 2) NOT NULL,
-    qtd_estoque INT NOT NULL,
+    ano_publicacao YEAR NOT NULL,
+    vl_preco DECIMAL(10, 2) NOT NULL CHECK (vl_preco >= 0),
+    qtd_estoque INT NOT NULL CHECK (qtd_estoque >= 0),
     id_autor INT NOT NULL,
     id_categoria INT NOT NULL,
     FOREIGN KEY (id_autor) REFERENCES autores(id_autor) ON DELETE RESTRICT,
@@ -38,9 +38,9 @@ CREATE TABLE livros (
 CREATE TABLE vendas (
     id_venda INT AUTO_INCREMENT PRIMARY KEY,
     nome_cliente VARCHAR(100) NOT NULL,
-    email_cliente VARCHAR(100) NOT NULL,
+    email_cliente VARCHAR(100) NOT NULL UNIQUE,
     id_livro INT NOT NULL,
-    quantidade INT NOT NULL,
+    quantidade INT NOT NULL CHECK (quantidade > 0),
     data_venda DATE NOT NULL,
     status_venda ENUM('finalizada', 'pendente', 'cancelada') DEFAULT 'pendente',
     FOREIGN KEY (id_livro) REFERENCES livros(id_livro) ON DELETE RESTRICT
